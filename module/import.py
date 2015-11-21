da18ed920b5cfc79ef67d88d7e2fc408b304c88a
fancyMore <hiafanti@gmail.com>
2015-11-21 10:35:27 +0800

    python

--- module/import.py
+++ module/import.py
@@ -1,11 +1,30 @@
 # -*- coding: UTF-8 -*-
 # -*Application Start*-
+#引用新功能
+from __future__ import division
+
+from __future__ import unicode_literals
 
 import os
-func = dir(os)
-print(func)
-print('more')
-print os.path.isdir(r'/data/webroot/resource/python')
-print os.path.isfile(r'/data/webroot/resource/python/test.txt')
+import math
+#动态导入模块
+try:
+    import json
+except ImportError:
+    import simplejson
+
+
+
+osFunc = dir(os)
+mathFunc = dir(math)
+
+print os.path.isdir(r'D:\python\excel')
+print os.path.isfile(r'D:\python\excel\demo.py')
+
+print json.dumps({'python':2.7})
+
+print (10/3)
+
+print isinstance('am I an unicode', unicode)
 
 # -*Application End*-
