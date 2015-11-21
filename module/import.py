e1991128ea6cf716ac26e89c9941714f64e03240
fancyMore <hiafanti@gmail.com>
2015-11-21 09:36:50 +0800

    print

--- module/import.py
+++ module/import.py
@@ -2,6 +2,9 @@
 # -*Application Start*-
 
 import os
+func = dir(os)
+print(func)
+print('hello world')
 print os.path.isdir(r'/data/webroot/resource/python')
 print os.path.isfile(r'/data/webroot/resource/python/test.txt')
 
