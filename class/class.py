ce198702c76f06f27d596292a4557d69c305f629
fancyMore <hiafanti@gmail.com>
2015-11-21 18:37:04 +0800

    class

---
+++ class/class.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+# -*Application Start*-
+class Animal():
+    this.name = 'banner'
+
+xiaoming = Animal()
+xiaohong = Animal()
+
+print (xiaoming.name)
+
+# -*Application End*-
