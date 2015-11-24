afe702982e0d1121d8f28b1b312de1ad499f52be
fancyMore <hiafanti@gmail.com>
2015-11-24 18:44:50 +0800

    dir

--- file/file_batch_name.py
+++ file/file_batch_name.py
@@ -1,18 +1,18 @@
 # -*- coding: UTF-8 -*-
 # -*Application Start*-
-import os															# Load the library module
-import sys															# Load the library module
+import os
+import sys
 
-work_dir=sys.argv[1]											# Set the variable work_dir with the first argument passed
-old_ext=sys.argv[2]											# Set the variable work_dir with the first argument passed
-new_ext=sys.argv[3]											# Set the variable work_dir with the first argument passed
+work_dir=sys.argv[1]
+old_ext=sys.argv[2]
+new_ext=sys.argv[3]
 
-files = os.listdir(work_dir)									# Set the variable files, by listing everything in the directory
-for filename in files:											# Loop through the files
-  file_ext = os.path.splitext(filename)[1]				# Get the file extension
-  if old_ext == file_ext:										# Start of the logic to check the file extensions, if old_ext = file_ext
-    newfile = filename.replace(old_ext, new_ext)	    # Set newfile to be the filename, replaced with the new extension
-    os.rename(													# Write the files
+files = os.listdir(work_dir)
+for filename in files:
+  file_ext = os.path.splitext(filename)[1]
+  if old_ext == file_ext:
+    newfile = filename.replace(old_ext, new_ext)
+    os.rename(
 	    os.path.join(work_dir, filename),
 		os.path.join(work_dir, newfile))
 # -*Application End*-
