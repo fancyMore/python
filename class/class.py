5755bba8ab4ecf93241b81b3019bdc31795eb71a
fancyMore <hiafanti@gmail.com>
2015-11-24 11:33:30 +0800

    class

--- class/class.py
+++ class/class.py
@@ -1,11 +1,16 @@
 # -*- coding: UTF-8 -*-
 # -*Application Start*-
-class Animal():
-    this.name = 'banner'
+class Berbon(object):
+    def __init__(self, name, type):
+        self.name = name
+        self.type = type
+    def say(self):
+        print (self.name)
+        print (self.type)
 
-xiaoming = Animal()
-xiaohong = Animal()
+appCCB = Berbon('建设银行', 'O2O')
+appBeiLong = Berbon('贝利龙', 'C2C')
 
-print (xiaoming.name)
-
+appCCB.say()
+appBeiLong.say()
 # -*Application End*-
