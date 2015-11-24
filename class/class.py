76d3ff3b1a649d1b9fd5edd386fa7710fc4381d2
fancyMore <hiafanti@gmail.com>
2015-11-24 15:19:29 +0800

    class

--- class/class.py
+++ class/class.py
@@ -5,17 +5,19 @@ class Berbon(object):
     def __init__(self, name, type):
         self.name = name
         self.type = type
-        self.__def = 'FED'#不可访问属性
+        self.__dev = 'FED'#不可访问属性
     def say(self):
         print (self.name)
         print (self.type)
+        print (self.__dev)
 
 appCCB = Berbon('建设银行', 'O2O')
 appBeiLong = Berbon('贝利龙', 'C2C')
 
 appCCB.say()
+print ('====================')
 appBeiLong.say()
-
+print ('====================')
 #rint (appCCB.__dev)
 print (appCCB.location)
 # -*Application End*-
