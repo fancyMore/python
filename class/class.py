09717eac22878d8f5b07228580b21bccb4b877b8
fancyMore <hiafanti@gmail.com>
2015-11-24 11:47:59 +0800

    clss

--- class/class.py
+++ class/class.py
@@ -1,9 +1,11 @@
 # -*- coding: UTF-8 -*-
 # -*Application Start*-
 class Berbon(object):
+    location = '天府大道中段'#类属性，实例共享
     def __init__(self, name, type):
         self.name = name
         self.type = type
+        self.__def = 'FED'#不可访问属性
     def say(self):
         print (self.name)
         print (self.type)
@@ -13,4 +15,7 @@ appBeiLong = Berbon('贝利龙', 'C2C')
 
 appCCB.say()
 appBeiLong.say()
+
+#rint (appCCB.__dev)
+print (appCCB.location)
 # -*Application End*-
