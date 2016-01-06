abed48d1e51e102839ec9dab5280a35634d9c6ea
fancyMore <hiafanti@gmail.com>
2016-01-06 11:02:52 +0800

    change

--- excel/group.py
+++ excel/group.py
@@ -8,13 +8,13 @@ import xlwt
 from xlutils.copy import copy
 
 # 发包方信息
-codeXlsPath = 'D:/handle/source/code.xls'
+codeXlsPath = 'D:/handle/source/code1.xls'
 codeBook = xlrd.open_workbook(codeXlsPath)
 codeSheet = codeBook.sheet_by_index(0)
 codeRows = codeSheet.nrows
 
 # 获取并处理数据源
-srcXlsPath = 'D:/handle/source/all.xls'
+srcXlsPath = 'D:/handle/source/source1.xls'
 srcBook = xlrd.open_workbook(srcXlsPath)
 srcSheet = srcBook.sheet_by_index(0)
 srcRows = srcSheet.nrows
@@ -29,8 +29,10 @@ for i in range(0,codeRows):
     row_data = codeSheet.row_values(i)
     myKey = row_data[0]
     class_code[myKey] = []
+    class_code[myKey].append(row_data[1])
     class_code[myKey].append(row_data[2])
     class_code[myKey].append(row_data[3])
+    class_code[myKey].append(row_data[4])
 
 # 分类存储相同数据
 class_list = {}
@@ -53,11 +55,21 @@ for d in class_list:
     for o in class_code:
         if o == d:
             msg = class_code[d][0]
+            stime = class_code[d][2]
+            pp = class_code[d][1]
+            sh = class_code[d][3]
     singleList = class_list[d]
     sLen = int(len(singleList)) #行数
     for i in range(0,sLen):
-        for k in range(0,9):
+        for k in range(0,10):
                 tmpSheet.write(3+i,k,singleList[i][k])
+    tmpSheet.write(1,1,msg.split('_')[2])
+    tmpSheet.write(1,2,msg.split('_')[3])
+    tmpSheet.write(1,4,stime)
+    tmpSheet.write(1,6,d)
+    tmpSheet.write(1,8,pp)
+    tmpSheet.write(1,10,sh)
+    tmpSheet.write(sLen+2,1,'集体'.decode('utf-8'))
     tmpBook.save('D:/handle/dest/'+ msg +  '_农户摸底信息表'.decode('UTF-8') +'.xls')
 #print len(class_list['51070420520101'])
 
