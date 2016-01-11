f63d61a3e24255a5484a20950cc3fc812153a34a
fancyMore <hiafanti@gmail.com>
2016-01-11 08:46:18 +0800

    python

--- excel/group.py
+++ excel/group.py
@@ -8,19 +8,19 @@ import xlwt
 from xlutils.copy import copy
 
 # 发包方信息
-codeXlsPath = 'D:/handle/source/code1.xls'
+codeXlsPath = 'D:/handle/source/taiping/code.xls'
 codeBook = xlrd.open_workbook(codeXlsPath)
 codeSheet = codeBook.sheet_by_index(0)
 codeRows = codeSheet.nrows
 
 # 获取并处理数据源
-srcXlsPath = 'D:/handle/source/source1.xls'
+srcXlsPath = 'D:/handle/source/taiping/source.xls'
 srcBook = xlrd.open_workbook(srcXlsPath)
 srcSheet = srcBook.sheet_by_index(0)
 srcRows = srcSheet.nrows
 
 # 结果数据
-destXlsPath = 'D:/handle/source/result_c.xls'
+destXlsPath = 'D:/handle/source/taiping/result.xls'
 destBook = xlrd.open_workbook(destXlsPath,formatting_info=True)
 
 #存储包信息
@@ -28,6 +28,7 @@ class_code = {}
 for i in range(0,codeRows):
     row_data = codeSheet.row_values(i)
     myKey = row_data[0]
+
     class_code[myKey] = []
     class_code[myKey].append(row_data[1])
     class_code[myKey].append(row_data[2])
@@ -53,6 +54,7 @@ for d in class_list:
     tmpBook = copy(destBook)
     tmpSheet = tmpBook.get_sheet(0)
     for o in class_code:
+
         if o == d:
             msg = class_code[d][0]
             stime = class_code[d][2]
@@ -63,12 +65,18 @@ for d in class_list:
     for i in range(0,sLen):
         for k in range(0,10):
                 tmpSheet.write(3+i,k,singleList[i][k])
+
+    txt = '填报单位：'.decode('utf-8') + msg.split('_')[2] + ' '.decode('utf-8') + msg.split('_')[3] + '   时间：'.decode('utf-8')  + stime + '   乡村组编码：'.decode('utf-8') + d + '   填报人：'.decode('utf-8') + pp +'   审核人：'.decode('utf-8') + sh
+    tmpSheet.write(1,0,txt)
+    '''
     tmpSheet.write(1,1,msg.split('_')[2])
     tmpSheet.write(1,2,msg.split('_')[3])
+
     tmpSheet.write(1,4,stime)
     tmpSheet.write(1,6,d)
     tmpSheet.write(1,8,pp)
     tmpSheet.write(1,10,sh)
+    '''
     tmpSheet.write(sLen+2,1,'集体'.decode('utf-8'))
     tmpBook.save('D:/handle/dest/'+ msg +  '_农户摸底信息表'.decode('UTF-8') +'.xls')
 #print len(class_list['51070420520101'])
